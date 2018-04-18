393f96250c30d128485597836066fc7ea53f0ae8
mferrufino <mferrufino@factumx.net>
2018-04-18 01:31:44 -0400

    Adding class

---
+++ FriendFinder.java
@@ -0,0 +1,70 @@
+
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * SD2x Homework #10
+ * Modify the method below so that it uses defensive programming.
+ * Please be sure not to change the method signature!
+ */
+
+public class FriendFinder {
+	
+	protected ClassesDataSource classesDataSource;
+	protected StudentsDataSource studentsDataSource;
+	
+	public FriendFinder(ClassesDataSource cds, StudentsDataSource sds) {
+		classesDataSource = cds;
+		studentsDataSource = sds;
+	}
+	
+	
+	public Set<String> findClassmates(Student theStudent) {
+		Set<String> classmates = new HashSet<String>();
+		if (theStudent == null) return null;
+
+		try {
+			String name = theStudent.getName();
+			if (name == null) return null;
+			try{
+				// find the classes that this student is taking
+				List<String> myClasses = classesDataSource.getClasses(name);
+				if (myClasses == null) return null;
+
+				// use the classes to find the names of the students
+				for (String myClass : myClasses) {
+					// list all the students in the class
+					List<Student> students = studentsDataSource.getStudents(myClass);
+					for (Student student : students) {
+						// find the other classes that they're taking
+						List<String> theirClasses = classesDataSource.getClasses(student.getName());
+						// see if all of the classes that they're taking are the same as the ones this student is taking
+						boolean same = true;
+						for (String c : myClasses) {
+							if (theirClasses.contains(c) == false) {
+								same = false;
+								break;
+							}
+						}
+						if (same) {
+							if (student.getName().equals(name) == false && classmates.contains(student.getName()) == false)
+								classmates.add(student.getName());
+						}
+					}
+				} // classes
+			}catch (NullPointerException ex) {
+				classmates = null;
+			}
+		}catch (IllegalArgumentException e) {
+			//e.printStackTrace();
+		}
+
+		if (classmates == null || classmates.isEmpty()) {
+			return null;
+		}
+		else return classmates;
+	}
+	
+
+}
